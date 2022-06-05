be13aa0ac70154d47d572731c58d0b0963407ebf
nvcbao2002 <106927247+nvcbao2002@users.noreply.github.com>
2022-06-06 01:31:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Em có thể làm ng anh không nhể😙',
+    desc: 'chọn đi bé ơi',
+    btnYes: 'Em đồng ý <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
